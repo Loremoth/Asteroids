de2bfe9e28b9f36719559038d825fc95f5780c92
= <=>
2021-05-03 00:14:38 +0200

    removed load image print

--- space_rocks/utils.py
+++ space_rocks/utils.py
@@ -10,7 +10,6 @@ def load_sound(name):
     return Sound(path)
 
 def load_sprite(name, with_alpha=True):
-    print(name)
     path = f"C:/Users/ActionICT/PycharmProjects/Asteroids/assets/sprites/{name}.png"
     loaded_sprite = load(path)
 
